1ee304f09b9a570bf6699e58db2b90ff543f8bb9
Max Kolotilkin <kolotilkin@gmail.com>
2017-11-03 09:26:18 +0200

    Added required utils for Recycled strategy

---
+++ src/main/java/com/wixpress/automation/webdriver/utils/functions/helpers/ThrowingRunnable.java
@@ -0,0 +1,23 @@
+package com.wixpress.automation.webdriver.utils.functions.helpers;
+
+
+import static com.wixpress.automation.func.ContainerUtils.safeInvoke;
+
+public interface ThrowingRunnable {
+
+    /**
+     * Performs required action
+     *
+     * @throws Throwable
+     */
+    void run() throws Throwable;
+
+    /**
+     * Returns a "silent" runnable, which performs given action and silences exceptions
+     *
+     * @return "silent" runnable
+     */
+    default Runnable silentRunnable() {
+        return () -> safeInvoke(this);
+    }
+}
---
+++ src/main/java/com/wixpress/automation/webdriver/utils/functions/helpers/ThrowingSupplier.java
@@ -0,0 +1,12 @@
+package com.wixpress.automation.webdriver.utils.functions.helpers;
+
+public interface ThrowingSupplier<T> {
+
+    /**
+     * Gets a result.
+     *
+     * @return a result
+     * @throws Throwable
+     */
+    T get() throws Throwable;
+}
